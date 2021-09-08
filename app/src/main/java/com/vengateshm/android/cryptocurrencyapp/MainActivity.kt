31c8f6b88e670727199cb0253dd02c49e3d370df
Vengatesh M <vengateshm.13@gmail.com>
2021-09-08 12:44:09 +0530

    status bar color change using side effect

--- app/src/main/java/com/vengateshm/android/cryptocurrencyapp/MainActivity.kt
+++ app/src/main/java/com/vengateshm/android/cryptocurrencyapp/MainActivity.kt
@@ -5,20 +5,38 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.vengateshm.android.cryptocurrencyapp.presentation.Screen
 import com.vengateshm.android.cryptocurrencyapp.presentation.coinDetail.CoinDetailScreen
 import com.vengateshm.android.cryptocurrencyapp.presentation.coinList.CoinListScreen
 import com.vengateshm.android.cryptocurrencyapp.ui.theme.CryptoCurrencyAppTheme
+import com.vengateshm.android.cryptocurrencyapp.ui.theme.PrimaryVariant
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Turn off the decor fitting system windows, which means we need to through handling
+        // insets
+        //WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
+
+            // Update the system bars to be translucent
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = MaterialTheme.colors.isLight
+
+            SideEffect {
+                systemUiController.setStatusBarColor(PrimaryVariant,
+                    darkIcons = useDarkIcons)
+            }
+
             CryptoCurrencyAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
